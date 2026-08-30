e6cf0d6f5ce0ebaf47165209d2f6e0db1d6d4eee
jungboh <jungboh@gmail.com>
2026-08-30 11:58:45 +0900

    상담 예약 차시를 6차까지 확대

---
+++ supabase/migrations/202608300002_expand_booking_slots_to_six.sql
@@ -0,0 +1,7 @@
+alter table public.consult_booking_reservations
+drop constraint if exists consult_booking_valid_slot;
+
+alter table public.consult_booking_reservations
+add constraint consult_booking_valid_slot
+check (slot_number between 1 and 6);
+
